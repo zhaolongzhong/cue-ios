085c932a7e1bbaf7869adf76f150f801d19d2a50
Zhaolong Zhong <zhlgzh@gmail.com>
2024-12-30 20:58:03 -0800

    Add more programming language highlight support

--- CueApp/Sources/CueApp/Utilities/LanguageDefinition.swift
+++ CueApp/Sources/CueApp/Utilities/LanguageDefinition.swift
@@ -16,6 +16,16 @@ public struct LanguageDefinition: Sendable {
 
 public struct LanguageDefinitions {
     public static let definitions: [String: LanguageDefinition] = [
+        "python": LanguageDefinition(
+            keywords: ["False", "None", "True", "and", "as", "assert", "async", "await", "break", "class",
+                      "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global",
+                      "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise",
+                      "return", "try", "while", "with", "yield"],
+            commentPattern: "#.*?$",
+            stringPattern: "(\"|'){1,3}[^\"']*?\\1{1,3}|(\"|'){1,3}.*?[^\\\\]\\2{1,3}",
+            functionPattern: "\\bdef\\s+([a-zA-Z_][a-zA-Z0-9_]*)"
+        ),
+
         "swift": LanguageDefinition(
             keywords: ["class", "struct", "enum", "protocol", "func", "var", "let", "if", "else", "guard",
                       "return", "true", "false", "nil", "self", "super", "init", "deinit", "get", "set",
@@ -29,14 +39,48 @@ public struct LanguageDefinitions {
             functionPattern: "\\bfunc\\s+([a-zA-Z_][a-zA-Z0-9_]*)"
         ),
 
-        "python": LanguageDefinition(
-            keywords: ["False", "None", "True", "and", "as", "assert", "async", "await", "break", "class",
-                      "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global",
-                      "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise",
-                      "return", "try", "while", "with", "yield"],
-            commentPattern: "#.*?$",
-            stringPattern: "(\"|'){1,3}[^\"']*?\\1{1,3}|(\"|'){1,3}.*?[^\\\\]\\2{1,3}",
-            functionPattern: "\\bdef\\s+([a-zA-Z_][a-zA-Z0-9_]*)"
+        "kotlin": LanguageDefinition(
+            keywords: ["as", "break", "class", "continue", "do", "else", "false", "for", "fun", "if", "in",
+                      "interface", "is", "null", "object", "package", "return", "super", "this", "throw",
+                      "true", "try", "typealias", "val", "var", "when", "while", "by", "catch", "constructor",
+                      "delegate", "dynamic", "field", "file", "finally", "get", "import", "init", "param",
+                      "property", "receiver", "set", "setparam", "where", "actual", "abstract", "annotation",
+                      "companion", "const", "crossinline", "data", "enum", "expect", "external", "final",
+                      "infix", "inner", "internal", "lateinit", "noinline", "open", "operator", "out",
+                      "override", "private", "protected", "public", "reified", "sealed", "suspend", "tailrec",
+                      "vararg", "field", "it"],
+            commentPattern: "\\/\\/.*?$|\\/\\*[\\s\\S]*?\\*\\/",
+            stringPattern: "\"\"\"[\\s\\S]*?\"\"\"|\"[^\"\\n]*\"|'[^'\\n]*'",
+            functionPattern: "\\bfun\\s+([a-zA-Z_][a-zA-Z0-9_]*)"
+        ),
+
+        "cpp": LanguageDefinition(
+            keywords: ["asm", "auto", "bool", "break", "case", "catch", "char", "class", "const",
+                      "const_cast", "continue", "default", "delete", "do", "double", "dynamic_cast",
+                      "else", "enum", "explicit", "export", "extern", "false", "float", "for", "friend",
+                      "goto", "if", "inline", "int", "long", "mutable", "namespace", "new", "operator",
+                      "private", "protected", "public", "register", "reinterpret_cast", "return", "short",
+                      "signed", "sizeof", "static", "static_cast", "struct", "switch", "template", "this",
+                      "throw", "true", "try", "typedef", "typeid", "typename", "union", "unsigned", "using",
+                      "virtual", "void", "volatile", "wchar_t", "while", "and", "and_eq", "bitand",
+                      "bitor", "compl", "not", "not_eq", "or", "or_eq", "xor", "xor_eq"],
+            commentPattern: "\\/\\/.*?$|\\/\\*[\\s\\S]*?\\*\\/",
+            stringPattern: "\"[^\"\\n]*\"|'[^'\\n]*'",
+            functionPattern: "\\b[A-Za-z0-9_]+\\s+([a-zA-Z_][a-zA-Z0-9_]*)\\s*\\("
+        ),
+
+        "go": LanguageDefinition(
+            keywords: ["break", "case", "chan", "const", "continue", "default", "defer", "else",
+                      "fallthrough", "for", "func", "go", "goto", "if", "import", "interface",
+                      "map", "package", "range", "return", "select", "struct", "switch", "type",
+                      "var", "true", "false", "iota", "nil", "int", "int8", "int16", "int32",
+                      "int64", "uint", "uint8", "uint16", "uint32", "uint64", "uintptr",
+                      "float32", "float64", "complex64", "complex128", "bool", "byte", "rune",
+                      "string", "error", "make", "len", "cap", "new", "append", "copy", "close",
+                      "delete", "complex", "real", "imag", "panic", "recover"],
+            commentPattern: "\\/\\/.*?$|\\/\\*[\\s\\S]*?\\*\\/",
+            stringPattern: "`[^`]*`|\"[^\"\\n]*\"|'[^'\\n]*'",
+            functionPattern: "\\bfunc\\s+([a-zA-Z_][a-zA-Z0-9_]*)"
         ),
 
         "javascript": LanguageDefinition(
@@ -88,6 +132,26 @@ public struct LanguageDefinitions {
             commentPattern: "\\/\\/.*?$|\\/\\*[\\s\\S]*?\\*\\/",
             stringPattern: "\"[^\"\\n]*\"|'[^'\\n]*'",
             functionPattern: "\\bfn\\s+([a-zA-Z_][a-zA-Z0-9_]*)"
+        ),
+
+        "csharp": LanguageDefinition(
+            keywords: ["abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char",
+                      "checked", "class", "const", "continue", "decimal", "default", "delegate", "do",
+                      "double", "else", "enum", "event", "explicit", "extern", "false", "finally",
+                      "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int",
+                      "interface", "internal", "is", "lock", "long", "namespace", "new", "null",
+                      "object", "operator", "out", "override", "params", "private", "protected",
+                      "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof",
+                      "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true",
+                      "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using",
+                      "virtual", "void", "volatile", "while", "add", "alias", "ascending",
+                      "async", "await", "by", "descending", "dynamic", "equals", "from", "get",
+                      "global", "group", "into", "join", "let", "nameof", "on", "orderby",
+                      "partial", "remove", "select", "set", "value", "var", "when", "where",
+                      "yield"],
+            commentPattern: "\\/\\/.*?$|\\/\\*[\\s\\S]*?\\*\\/",
+            stringPattern: "@\"[^\"]*\"|\"[^\"\\n]*\"|'[^'\\n]*'",
+            functionPattern: "\\b[A-Za-z0-9_<>]+\\s+([a-zA-Z_][a-zA-Z0-9_]*)\\s*\\("
         ),
 
         "plaintext": LanguageDefinition(
